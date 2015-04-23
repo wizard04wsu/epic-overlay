dbf0b19091418d9878e806871ff6663b5d0ab472
Andy Harrison <wizard04wsu@users.noreply.github.com>
2015-04-23 13:59:12 -0400

    Twitch template works

--- wwwroot/templates/twitch.php
+++ wwwroot/templates/twitch.php
@@ -7,6 +7,17 @@ header("Expires: -1");
 
 require '_getSettings.php';
 
+$volume; $video;
+
+if(!$errMsg){
+	
+	$volume = intval($settingsArr['volume']);
+	$volume = $volume < 0 ? 0 : $volume > 100 ? 100 : $volume;
+	
+	$video = $settingsArr['video'] ? '&videoId='.htmlspecialchars($settingsArr['video']) : '';
+	
+}
+
 ?><!DOCTYPE html>
 
 <html>
@@ -14,45 +25,25 @@ require '_getSettings.php';
 	
 	<meta charset="UTF-8">
 	
-	<title><?php echo $title; ?></title>
+	<title><?php echo $title ? htmlspecialchars($title) : 'Epic Overlay: Twitch player'; ?></title>
 	
 	<style type="text/css" media="all">
 		html, body {
 			margin: 0;
 			padding: 0;
 		}
-		html, body, #player {
-			width: 100%;
-			height: 100%;
+		html {
+			height:100%;
 		}
-		#player {
-			display: block;
+		body {
+			position:absolute;
+			top:0;
+			right:0;
+			bottom:-31px;
+			left:0;
+			overflow:hidden;
 		}
 	</style>
-	
-	<script type="text/javascript" src="https://ttv-api.s3.amazonaws.com/twitch.min.js"></script>
-	<script type="text/javascript">
-		
-		var settings = <?php echo $settingsJson; ?>;
-		
-		function initPlayer(){
-			
-			"use strict";
-			
-			Twitch.init({clientId: 'YOUR_CLIENT_ID_HERE'}, function (error, status){
-				
-				if(error){
-					console.log(error);
-					return;
-				}
-				
-				//the SDK is now loaded
-				
-			});
-			
-		}
-		
-	</script>
 	
 </head>
 <body>
@@ -62,10 +53,23 @@ if($errMsg){
 }
 else{
 ?>
-	<div id="player"></div>
-	<script type="text/javascript">
-		//
-	</script>
+	<object id="player" type="application/x-shockwave-flash" 
+			height="100%" 
+			width="100%" 
+			data="http://www.twitch.tv/widgets/live_embed_player.swf" 
+			bgcolor="#000000"
+			>
+		<param name="allowFullScreen" 
+			value="false" />
+		<param name="allowScriptAccess" 
+			value="always" />
+		<param name="allowNetworking" 
+			value="all" />
+		<param name="movie" 
+			value="http://www.twitch.tv/widgets/live_embed_player.swf" />
+		<param name="flashvars" 
+			value="channel=<?php echo htmlspecialchars($settingsArr['channel']) . $video; ?>&auto_play=true&start_volume=<?php echo $volume; ?>" />
+	</object>
 <?php
 }
 ?>
